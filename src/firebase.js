33114ad9316e727e62e10ec7ac09bc589d4361c8
skmtkytr <skmtkytr@gmail.com>
2017-12-06 20:13:32 +0900

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+<script src="https://www.gstatic.com/firebasejs/4.7.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDQVbjOPuAQWMlhGj0cB1iZUDceLyMsfs4",
+    authDomain: "test-go-app-e5eb9.firebaseapp.com",
+    databaseURL: "https://test-go-app-e5eb9.firebaseio.com",
+    projectId: "test-go-app-e5eb9",
+    storageBucket: "test-go-app-e5eb9.appspot.com",
+    messagingSenderId: "50803624477"
+  };
+  firebase.initializeApp(config);
+</script>
